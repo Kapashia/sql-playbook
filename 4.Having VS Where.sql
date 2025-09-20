cd18c56a529a601b413cf64644f5031430b38ade
Kapashia binte giash <nabu.3797@gmail.com>
2025-09-20 16:56:53 +0600

    Update 4.Having VS Where.sql

--- 4.Having VS Where.sql
+++ 4.Having VS Where.sql
@@ -22,11 +22,7 @@ FROM employee_demographics
 GROUP BY gender
 HAVING  AVG(age) > 40
 ;
-SELECT gender, AVG(age) as AVG_age
-FROM employee_demographics
-GROUP BY gender
-HAVING AVG(age) > 40
-;
+
 
 
 
